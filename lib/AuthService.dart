d61f1c75f9a83e5082d732ce7e657c656b9fb634
NaveenKumar-07 <78958995+NaveenKumar-07@users.noreply.github.com>
2021-02-12 16:59:30 +0530

    Update AuthService.dart

--- lib/AuthService.dart
+++ lib/AuthService.dart
@@ -2,7 +2,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_facebook_login/flutter_facebook_login.dart';
 import 'package:flutter_twitter_login/flutter_twitter_login.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
@@ -24,11 +23,6 @@ class AuthService {
           accessToken: googleAuth.accessToken,
         );
         User user = (await _auth.signInWithCredential(credential)).user;
-        final SharedPreferences sharedPreferences =
-            await SharedPreferences.getInstance();
-        sharedPreferences.setString('email', _auth.currentUser.email);
-        sharedPreferences.setString('name', _auth.currentUser.displayName);
-        sharedPreferences.setString('purl', _auth.currentUser.photoURL);
         print(user.providerData);
       }
     } catch (e) {
@@ -40,11 +34,6 @@ class AuthService {
     final AuthCredential credential =
         TwitterAuthProvider.credential(accessToken: token, secret: secret);
     User user = (await _auth.signInWithCredential(credential)).user;
-    final SharedPreferences sharedPreferences =
-        await SharedPreferences.getInstance();
-    sharedPreferences.setString('email', _auth.currentUser.email);
-    sharedPreferences.setString('name', _auth.currentUser.displayName);
-    sharedPreferences.setString('purl', _auth.currentUser.photoURL);
     print(user.providerData);
   }
 
